779863681e989f7d58ce6a781d09b5b89e804532
EkoBean <douzihua02@gmail.com>
2026-04-02 21:11:57 +0800

    更新幫助頁面，新增 /troller 指令的詳細說明及參數選項

--- src/commands/utility/troller.js
+++ src/commands/utility/troller.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, MessageFlags, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, MessageFlags } = require('discord.js');
 const { playerSearch } = require('../../utils/playerSearch.js');
 const { fflogsSearch } = require('../../utils/fflogsSearch.js');
 const path = require('node:path');
@@ -9,106 +9,106 @@ const template = JSON.parse(fs.readFileSync(templatePath, 'utf8'));
 
 
 module.exports = {
-    data: new SlashCommandBuilder().setName('troller')
-        .setDescription('Catch a troller into pokemon bank.')
-        .addIntegerOption(
-            option => option.setName('lodestone_id')
-                .setDescription('The troller\'s lodestone URL ID.')
-                .setDescriptionLocalizations(
-                    {
-                        'zh-TW': '輸入戳仔的Lodestone號碼。',
-                    }).setRequired(true))
-        .addStringOption(
-            option => option.setName('comment')
-                .setDescription('A comment about this player. (Optional)')
-                .setDescriptionLocalizations(
-                    {
-                        'zh-TW': '備註訊息（選填）',
-                    }).setRequired(false))
-        .addAttachmentOption(
-            option => option.setName('screenshot1')
-                .setDescription('Upload screenshots (3 at most)')
-                .setDescriptionLocalizations(
-                    {
-                        'zh-TW': '上傳截圖（最多3張）',
-                    }).setRequired(false))
-        .addAttachmentOption(
-            option => option.setName('screenshot2')
-                .setDescription('Upload screenshots (3 at most)')
-                .setDescriptionLocalizations(
-                    {
-                        'zh-TW': '上傳截圖（最多3張）',
-                    }).setRequired(false))
-        .addAttachmentOption(
-            option => option.setName('screenshot3')
-                .setDescription('Upload screenshots (3 at most)')
-                .setDescriptionLocalizations(
-                    {
-                        'zh-TW': '上傳截圖（最多3張）',
-                    }).setRequired(false)),
-    cooldown: 5,
-    dev: true,
-    async execute(interaction) {
-        const lodestoneUrl = 'https://na.finalfantasyxiv.com/lodestone/character/'
-        const locale = interaction.locale || interaction.user?.locale || 'en-US';
-        const lodestoneId = interaction.options.getInteger('lodestone_id');
-        const comment = interaction.options.getString('comment') || '';
-        const screenshot1 = interaction.options.getAttachment('screenshot1');
-        const screenshot2 = interaction.options.getAttachment('screenshot2');
-        const screenshot3 = interaction.options.getAttachment('screenshot3');
-        const playerInfo = await playerSearch(lodestoneId);
+	data: new SlashCommandBuilder().setName('troller')
+		.setDescription('Catch a troller into pokemon bank.')
+		.addIntegerOption(
+			option => option.setName('lodestone_id')
+				.setDescription('The troller\'s lodestone URL ID.')
+				.setDescriptionLocalizations(
+					{
+						'zh-TW': '輸入戳仔的Lodestone號碼。',
+					}).setRequired(true))
+		.addStringOption(
+			option => option.setName('comment')
+				.setDescription('A comment about this player. (Optional)')
+				.setDescriptionLocalizations(
+					{
+						'zh-TW': '備註訊息（選填）',
+					}).setRequired(false))
+		.addAttachmentOption(
+			option => option.setName('screenshot1')
+				.setDescription('Upload screenshots (3 at most)')
+				.setDescriptionLocalizations(
+					{
+						'zh-TW': '上傳截圖（最多3張）',
+					}).setRequired(false))
+		.addAttachmentOption(
+			option => option.setName('screenshot2')
+				.setDescription('Upload screenshots (3 at most)')
+				.setDescriptionLocalizations(
+					{
+						'zh-TW': '上傳截圖（最多3張）',
+					}).setRequired(false))
+		.addAttachmentOption(
+			option => option.setName('screenshot3')
+				.setDescription('Upload screenshots (3 at most)')
+				.setDescriptionLocalizations(
+					{
+						'zh-TW': '上傳截圖（最多3張）',
+					}).setRequired(false)),
+	cooldown: 5,
+	dev: false,
+	async execute(interaction) {
+		const lodestoneUrl = 'https://na.finalfantasyxiv.com/lodestone/character/';
+		const locale = interaction.locale || interaction.user?.locale || 'en-US';
+		const lodestoneId = interaction.options.getInteger('lodestone_id');
+		const comment = interaction.options.getString('comment') || '';
+		const screenshot1 = interaction.options.getAttachment('screenshot1');
+		const screenshot2 = interaction.options.getAttachment('screenshot2');
+		const screenshot3 = interaction.options.getAttachment('screenshot3');
+		const playerInfo = await playerSearch(lodestoneId);
 
-        const reporter = { name: interaction.user.username, avatar: interaction.user.displayAvatarURL() };
+		const reporter = { name: interaction.user.username, avatar: interaction.user.displayAvatarURL() };
 
-        if (!playerInfo) {
-            await interaction.reply(locale === 'zh-TW' ?
-                {
-                    content: '無法找到該玩家的資料，請確認Lodestone ID是否正確。',
-                    flags: MessageFlags.Ephemeral,
-                } : {
-                    content: 'Unable to find player information. Please check if the Lodestone ID is correct.',
-                    flags: MessageFlags.Ephemeral,
-                });
-            return;
-        }
-        
-        const fflogsLink = await fflogsSearch(playerInfo.name, playerInfo.world, playerInfo.dc);
+		if (!playerInfo) {
+			await interaction.reply(locale === 'zh-TW' ?
+				{
+					content: '無法找到該玩家的資料，請確認Lodestone ID是否正確。',
+					flags: MessageFlags.Ephemeral,
+				} : {
+					content: 'Unable to find player information. Please check if the Lodestone ID is correct.',
+					flags: MessageFlags.Ephemeral,
+				});
+			return;
+		}
 
-        const embedData = JSON.parse(JSON.stringify(template.embed));
-        embedData.title = playerInfo.name;
-        embedData.url = lodestoneUrl + lodestoneId;
-        embedData.timestamp = new Date().toISOString();
-        embedData.footer.text = reporter.name;
-        embedData.footer.icon_url = reporter.avatar;
-        embedData.thumbnail.url = playerInfo.avatar;
-        embedData.description = comment;
-        embedData.fields[0].value = lodestoneId;
-        embedData.fields[1].value = `[${playerInfo?.name}](${fflogsLink})`;
+		const fflogsLink = await fflogsSearch(playerInfo.name, playerInfo.world, playerInfo.dc);
+
+		const embedData = JSON.parse(JSON.stringify(template.embed));
+		embedData.title = playerInfo.name;
+		embedData.url = lodestoneUrl + lodestoneId;
+		embedData.timestamp = new Date().toISOString();
+		embedData.footer.text = reporter.name;
+		embedData.footer.icon_url = reporter.avatar;
+		embedData.thumbnail.url = playerInfo.avatar;
+		embedData.description = comment;
+		embedData.fields[0].value = lodestoneId;
+		embedData.fields[1].value = `[${playerInfo?.name}](${fflogsLink})`;
 
 
-        const images = [screenshot1?.url, screenshot2?.url, screenshot3?.url].filter(Boolean);
-        if (locale === 'zh-TW') {
-            await interaction.reply(
-                {
-                    content: `
+		const images = [screenshot1?.url, screenshot2?.url, screenshot3?.url].filter(Boolean);
+		if (locale === 'zh-TW') {
+			await interaction.reply(
+				{
+					content: `
                     **收服到寶可夢了！**
                     `,
-                    embeds: [embedData],
-                    files: images,
-                },
-            );
-        }
-        else {
-            await interaction.reply(
-                {
-                    content: `
+					embeds: [embedData],
+					files: images,
+				},
+			);
+		}
+		else {
+			await interaction.reply(
+				{
+					content: `
                     **Caught a Pokémon!**
                     `,
-                    embeds: [embedData],
-                    files: images,
-                },
+					embeds: [embedData],
+					files: images,
+				},
 
-            );
-        }
-    },
+			);
+		}
+	},
 };
