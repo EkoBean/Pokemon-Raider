a3a8ebb0717dec8d8a61a671f6ac11688386659f
EkoBean <douzihua02@gmail.com>
2026-04-20 12:07:37 +0800

    node stone bug reversion

--- src/commands/sheets/refreshList.js
+++ src/commands/sheets/refreshList.js
@@ -1,20 +1,20 @@
-const { SlashCommandBuilder, MessageFlags } = require('discord.js');
-const { playerSearch } = require('../../utils/playerSearch.js');
-const { fflogsSearch } = require('../../utils/fflogsSearch.js');
-const { cooldown } = require('../utility/troller.js');
+// const { SlashCommandBuilder, MessageFlags } = require('discord.js');
+// const { playerSearch } = require('../../utils/playerSearch.js');
+// const { fflogsSearch } = require('../../utils/fflogsSearch.js');
+// const { cooldown } = require('../utility/troller.js');
 
 
-module.exports = {
-	data: new SlashCommandBuilder().setName('refreshlist')
-		.setDescription('Refresh the player name and links in GOOGLE Sheets.')
-		.setDescriptionLocalizations(
-			{
-				'zh-TW': '更新GOOGLE表單玩家名稱及連結。',
-			},
-		),
-	cooldown: 10,
-	async execute(interaction) { 
+// module.exports = {
+// 	data: new SlashCommandBuilder().setName('refreshlist')
+// 		.setDescription('Refresh the player name and links in GOOGLE Sheets.')
+// 		.setDescriptionLocalizations(
+// 			{
+// 				'zh-TW': '更新GOOGLE表單玩家名稱及連結。',
+// 			},
+// 		),
+// 	cooldown: 10,
+// 	async execute(interaction) { 
         
-    },
+//     },
 
-};
+// };
